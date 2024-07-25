f7111a2ef8174f66838e4be732f6cbdd3bf19e23
mazelcar <mazelcar2018@gmail.com>
2024-07-25 13:53:12 -0600

    Set up project structure and initial files

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+﻿from flask import Flask, jsonify
+import boto3
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("KPIs")
+
+@app.route("/api/kpis")
+def get_kpis():
+    response = table.scan()
+    return jsonify(response["Items"])
+
+if __name__ == "__main__":
+    app.run(debug=True)
